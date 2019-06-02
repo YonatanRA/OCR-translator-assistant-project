db8be739f5bb76d05650fabd9aaaf2c971a47deb
YonatanRA <yeho@mail.com>
2019-06-02 18:31:31 +0200

    con cnn

--- src/ejecucion.py
+++ src/ejecucion.py
@@ -6,17 +6,22 @@ from ocr_pipeline import *
 
 
 if __name__=='__main__':
-	A_opt=pd.read_csv('alfa-letras.csv').values
 	captura()
 	contraste()
 	idx=contorno()
-	palabra=interpreta(idx, A_opt).lower()
+	#palabra=interpreta_softmax(idx).lower()        # con modelo softmax
+	palabra=interpreta_cnn(idx).lower()             # con modelo convolucional
 	print (palabra)
 	habla(palabra, leng='es')
 	idioma='en'
 	traduccion=(traduce(palabra.lower(), leng=idioma))
 	print (traduccion)
 	habla(traduccion, leng=idioma)
+
+
+
+
+
 
 
 
--- src/ocr_pipeline.py
+++ src/ocr_pipeline.py
@@ -6,6 +6,19 @@ import numpy as np                   # numerical python
 import pandas as pd                  # dataframe (datos parametros)
 from google_speech import Speech     # para hablar
 from googletrans import Translator   # para traducir
+import os                            # sistema, directorios
+from keras.models import load_model  # carga modelo de keras
+import tensorflow as tf              # quitar texto de tensorflow
+tf.logging.set_verbosity(tf.logging.ERROR)
+import pymongo                       # mongodb, para llamada a atlas
+
+
+
+
+def token(token):                # lectura de txt, token
+    with open(token, 'r') as f:
+        t=f.readlines()[0].split('\n')[0]
+    return t
 
 
 
@@ -92,11 +105,21 @@ def f(X,a):                                 # funcion logistica, sigmoide, funci
 
 
 
-def interpreta(idx, A_opt):
+def interpreta_softmax(idx):                     # funcion evaluacion modelo softmax
+	A_opt=pd.read_csv('alfa-letras.csv').values  # parametros salida softmax
 	resultado=''
-	alfabeto={1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J',
-	          11:'K', 12:'L', 13:'M', 14:'N', 15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T',
-	          21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
+	
+	alfabeto={0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9',
+		      10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F', 16:'G', 17:'H', 18:'I', 19:'J',
+	          20:'K', 21:'L', 22:'M', 23:'N', 24:'O', 25:'P', 26:'Q', 27:'R', 28:'S', 29:'T',
+	          30:'U', 31:'V', 32:'W', 33:'X', 34:'Y', 35:'Z',
+	          36:'a', 37:'b', 38:'c', 39:'d', 40:'e', 41:'f', 42:'g', 43:'h', 44:'i', 45:'j',
+	          46:'k', 47:'l', 48:'m', 49:'n', 50:'o', 51:'p', 52:'q', 53:'r', 54:'s', 55:'t',
+	          56:'u', 57:'v', 58:'w', 59:'x', 60:'y', 61:'z'}
+	
+	alfabeto_M={1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J',
+	            11:'K', 12:'L', 13:'M', 14:'N', 15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T',
+	            21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
 	          
 	for i in range(idx):
 		nombre=str(i+1)+'.png'           # nombre de la imagen
@@ -121,17 +144,53 @@ def interpreta(idx, A_opt):
 			probs[n,0]=n                  # evaluacion de la prediccion
 			probs[n,1]=f(datos,alfa)      
 		probs=probs[probs[:,1].argsort()[::-1]]	 # primero el de mas probabilidad
-		etiqueta=int(probs[0,0])                 # etiqueta de la letra
-		resultado+=alfabeto[etiqueta]
-
+		etiqueta=int(probs[0,0])                 # prediccion, etiqueta de la letra
+		resultado+=alfabeto_M[etiqueta]          # busca en alfabeto
+	
+	for i in range(idx):        # borra imagenes
+		nombre=str(i+1)+'.png' 
+		os.remove(nombre)                           
 	return resultado
+
+
+
+def interpreta_cnn(idx):                 # funcion evaluacion modelo cnn
+	modelo=load_model('modelo_cnn_letras.h5')   # modelo cnn
+	resultado=''
+	alfabeto={0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9',
+		      10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F', 16:'G', 17:'H', 18:'I', 19:'J',
+	          20:'K', 21:'L', 22:'M', 23:'N', 24:'O', 25:'P', 26:'Q', 27:'R', 28:'S', 29:'T',
+	          30:'U', 31:'V', 32:'W', 33:'X', 34:'Y', 35:'Z',
+	          36:'a', 37:'b', 38:'c', 39:'d', 40:'e', 41:'f', 42:'g', 43:'h', 44:'i', 45:'j',
+	          46:'k', 47:'l', 48:'m', 49:'n', 50:'o', 51:'p', 52:'q', 53:'r', 54:'s', 55:'t',
+	          56:'u', 57:'v', 58:'w', 59:'x', 60:'y', 61:'z'}
+	
+	alfabeto_M={1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J',
+	            11:'K', 12:'L', 13:'M', 14:'N', 15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T',
+	            21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
+	
+	for i in range(idx):
+		nombre=str(i+1)+'.png'           # nombre de la imagen
+		img=cv2.imread(nombre)           # lee imagen
+		datos=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		datos=np.array(Image.fromarray(datos).resize((28,28)))
+		datos=(255-datos)/255              # normliza y negativo imagen
+		datos=datos.reshape(1,28,28,1)     # redimensiona para input
+		pred=modelo.predict_classes(datos) # prediccion
+		resultado+=alfabeto_M[pred[0]]  
+		
+	for i in range(idx):        # borra imagenes
+		nombre=str(i+1)+'.png' 
+		os.remove(nombre) 	     
+	return resultado
+
 
 
 
 
 def habla(texto, leng='es'):
 	voz=Speech(texto, leng)
-	return voz.play()
+	return voz.play() 
 
 
 
@@ -143,5 +202,49 @@ def traduce(texto, leng='en'):
 	pronunciacion=traductor.translate(texto, dest=leng).pronunciation
 	return traduccion
 
+
+
+
+
+def mongo_escribe(ori, trad):   # llamada a atlas
+	cliente=pymongo.MongoClient('mongodb+srv://Yonatan:{}@mambacluster-v9uol.mongodb.net/test?retryWrites=true&w=majority'.format(token('mongoatlas.txt')))
+	db=cliente.test  # base de datos
+
+	try:
+		print('MongoDB version is {}'.format(cliente.server_info()['version']))
+	except pymongo.errors.OperationFailure as error:
+		print(error)
+		quit(1)
+
+	original=db.original      # colecciones
+	traduccion=db.traduccion
+
+	cursor=original.find()    # cursores
+	cursor2=traduccion.find()
+
+	cont=0                   # comprueba si existe el original, si no añadela
+	for item in cursor:
+		if item['palabra']==ori: cont+=1
+	if cont==0: original.insert_one({'palabra':ori})
+
+	cont2=0                  # comprueba si existe la traduccion, si no añadela
+	for item2 in cursor2:
+		if item2['palabra']==trad: cont2+=1
+	if cont2==0: traduccion.insert_one({'palabra':trad})
+
+
+
+
+def mongo_lee():
+	cliente=pymongo.MongoClient('mongodb+srv://Yonatan:{}@mambacluster-v9uol.mongodb.net/test?retryWrites=true&w=majority'.format(token('mongoatlas.txt')))
+	db=cliente.test           # base de datos
+	original=db.original      # colecciones
+	traduccion=db.traduccion
+	cursor=original.find()    # cursores
+	cursor2=traduccion.find()
+	for item in cursor:
+		print(item['palabra'])
+	for item2 in cursor2:
+		print(item2['palabra'])
 
 
